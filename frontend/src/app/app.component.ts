68ec11356166795952e87fc9cc3d1b284a393bb0
Marian Mutu <48853386+Marian2910@users.noreply.github.com>
2025-11-15 19:38:04 +0200

    Add initial spark_jobs module + some dashboard adjustments (#26)
    * Add initial spark_jobs module + some dashboard adjustments
    
    * Enhance sidenav functionality and styling with collapsible feature and improved transitions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,13 +4,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {CustomSidenavComponent} from './components/custom-sidenav/custom-sidenav.component';
-import {YoutubeDataContentComponent} from './youtube-data-content/youtube-data-content.component';
+import { CustomSidenavComponent } from './components/custom-sidenav/custom-sidenav.component';
+import { YoutubeDataContentComponent } from './youtube-data-content/youtube-data-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [YoutubeDataContentComponent, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent],
+  imports: [
+    YoutubeDataContentComponent,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    CustomSidenavComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isSidenavCollapsed = false;
+
+  onSidenavCollapsedChange(collapsed: boolean) {
+    this.isSidenavCollapsed = !!collapsed;
+  }
+}
