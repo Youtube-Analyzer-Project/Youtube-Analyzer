7bedee1a94f2c36dd30201d1d66554d81227fa37
Forminte Andrei-Gabriel <91501076+andrey100f@users.noreply.github.com>
2025-12-11 22:35:17 +0200

    Sentiment Analysis Page (#51)
    * Implement sentiment analysis service and update widgets for sentiment summary display
    
    * Add sentiment line chart component and integrate with sentiment analysis page
    
    * Add sentiment pie chart component and integrate into sentiment analysis page
    
    * Add video list component to sentiment analysis page and integrate sentiment video data
    
    * Add format number pipe and update chart colors in sentiment analysis components
    
    * UI improvments and fixes
    
    * pie chart with default border
    
    ---------
    
    Co-authored-by: Marian Mutu <marian.mutu02@gmail.com>

---
+++ frontend/src/app/pages/sentiment-analysis/charts/sentiment-pie-chart/sentiment-pie-chart.component.ts
@@ -0,0 +1,50 @@
+import { Component, ElementRef, input, OnDestroy, OnInit, viewChild } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-sentiment-pie-chart',
+  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle],
+  templateUrl: './sentiment-pie-chart.component.html',
+  styleUrl: './sentiment-pie-chart.component.scss',
+})
+export class SentimentPieChartComponent implements OnInit, OnDestroy {
+  data = input.required<number[]>();
+
+  protected chart = viewChild.required<ElementRef>('chart');
+
+  ngOnInit(): void {
+    new Chart(this.chart().nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['Positive', 'Neutral', 'Negative'],
+        datasets: [
+          {
+            label: '',
+            data: this.data(),
+            backgroundColor: ['#DD4046', '#499F5A', '#F3BC24'],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'right',
+            labels: {
+              usePointStyle: true,
+              pointStyle: 'circle',
+              padding: 20,
+            },
+          },
+        },
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.chart().nativeElement.remove();
+  }
+}
--- frontend/src/app/pages/sentiment-analysis/sentiment-analysis.component.ts
+++ frontend/src/app/pages/sentiment-analysis/sentiment-analysis.component.ts
@@ -1,28 +1,212 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal, OnDestroy } from '@angular/core';
 import { WidgetComponent } from '../../components/widget/widget.component';
-import { Widget } from '../../types/dashboard.type';
-import { DashboardService } from '../../services/dashboard.service';
-import { MatToolbar } from '@angular/material/toolbar';
+import { SentimentApiService } from '../../services/sentiment-api.service';
+import { Subscription } from 'rxjs';
+import {
+  SentimentChartSeriesDto,
+  SentimentSummaryDto,
+  SentimentVideoDto,
+  Widget,
+} from '../../types/sentiment.types';
+import { SentimentLineChartComponent } from './charts/sentiment-line-chart/sentiment-line-chart.component';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { SentimentPieChartComponent } from './charts/sentiment-pie-chart/sentiment-pie-chart.component';
+import { VideoListComponent } from './video-list/video-list.component';
+
+// TODO: Remove mock data when added more sentiment data
+const MOCK_SERIES: SentimentChartSeriesDto[] = [
+  // -------- CATEGORY 1 --------
+  { date: '2025-12-10', category_id: '1', avg_score: 0.08 },
+  { date: '2025-12-11', category_id: '1', avg_score: 0.12 },
+  { date: '2025-12-12', category_id: '1', avg_score: -0.03 },
+  { date: '2025-12-13', category_id: '1', avg_score: 0.18 },
+  { date: '2025-12-14', category_id: '1', avg_score: 0.25 },
+  { date: '2025-12-15', category_id: '1', avg_score: 0.09 },
+  { date: '2025-12-16', category_id: '1', avg_score: -0.14 },
+  { date: '2025-12-17', category_id: '1', avg_score: 0.04 },
+  { date: '2025-12-18', category_id: '1', avg_score: 0.31 },
+  { date: '2025-12-19', category_id: '1', avg_score: 0.27 },
+
+  // -------- CATEGORY 2 --------
+  { date: '2025-12-10', category_id: '2', avg_score: -0.12 },
+  { date: '2025-12-11', category_id: '2', avg_score: -0.05 },
+  { date: '2025-12-12', category_id: '2', avg_score: 0.02 },
+  { date: '2025-12-13', category_id: '2', avg_score: 0.15 },
+  { date: '2025-12-14', category_id: '2', avg_score: 0.22 },
+  { date: '2025-12-15', category_id: '2', avg_score: 0.18 },
+  { date: '2025-12-16', category_id: '2', avg_score: 0.05 },
+  { date: '2025-12-17', category_id: '2', avg_score: -0.02 },
+  { date: '2025-12-18', category_id: '2', avg_score: 0.11 },
+  { date: '2025-12-19', category_id: '2', avg_score: 0.09 },
+
+  // -------- CATEGORY 3 --------
+  { date: '2025-12-10', category_id: '3', avg_score: 0.32 },
+  { date: '2025-12-11', category_id: '3', avg_score: 0.28 },
+  { date: '2025-12-12', category_id: '3', avg_score: 0.2 },
+  { date: '2025-12-13', category_id: '3', avg_score: 0.14 },
+  { date: '2025-12-14', category_id: '3', avg_score: 0.1 },
+  { date: '2025-12-15', category_id: '3', avg_score: 0.06 },
+  { date: '2025-12-16', category_id: '3', avg_score: -0.01 },
+  { date: '2025-12-17', category_id: '3', avg_score: 0.04 },
+  { date: '2025-12-18', category_id: '3', avg_score: 0.09 },
+  { date: '2025-12-19', category_id: '3', avg_score: 0.13 },
+];
 
 @Component({
   selector: 'app-sentiment-analysis',
   standalone: true,
-  imports: [WidgetComponent, MatToolbar],
-  providers: [DashboardService],
+  imports: [
+    WidgetComponent,
+    SentimentLineChartComponent,
+    MatProgressSpinner,
+    SentimentPieChartComponent,
+    VideoListComponent,
+  ],
   templateUrl: './sentiment-analysis.component.html',
   styleUrl: './sentiment-analysis.component.scss',
 })
-export class SentimentAnalysisComponent implements OnInit {
-  private _dashboardService = inject(DashboardService);
+export class SentimentAnalysisComponent implements OnInit, OnDestroy {
+  private _sentimentService = inject(SentimentApiService);
+  private _sentimentSummarySubscription: Subscription = new Subscription();
+  private _sentimentChartsSubscription: Subscription = new Subscription();
+  private _positiveVideosSubscription: Subscription = new Subscription();
+  private _neutralVideosSubscription: Subscription = new Subscription();
+  private _negativeVideosSubscription: Subscription = new Subscription();
 
+  protected chartIsLoaded = signal<boolean>(false);
+  protected videosAreLoaded = signal<boolean>(false);
   protected widgets = signal<Widget[]>([]);
-  protected charts = signal<Widget[]>([]);
+  protected lineChartData = signal<SentimentChartSeriesDto[]>([]);
+  protected dataset1 = signal<SentimentChartSeriesDto[]>([]);
+  protected dataset2 = signal<SentimentChartSeriesDto[]>([]);
+  protected dataset3 = signal<SentimentChartSeriesDto[]>([]);
+  protected sentimentDistributions = signal<number[]>([]);
+  protected positiveVideos = signal<SentimentVideoDto[]>([]);
+  protected negativeVideos = signal<SentimentVideoDto[]>([]);
 
   ngOnInit(): void {
     this._getWidgets();
+    this._getChartData();
+    this._getSentimentVideos();
+  }
+
+  ngOnDestroy(): void {
+    this._sentimentSummarySubscription.unsubscribe();
+    this._sentimentChartsSubscription.unsubscribe();
+    this._positiveVideosSubscription.unsubscribe();
+    this._neutralVideosSubscription.unsubscribe();
+    this._negativeVideosSubscription.unsubscribe();
   }
 
   private _getWidgets(): void {
-    this.widgets.set(this._dashboardService.getWidgets());
+    this._sentimentSummarySubscription = this._sentimentService
+      .getSentimentSummary()
+      .subscribe((data: SentimentSummaryDto) => {
+        const sentiment =
+          data.overall_sentiment_label === 'Positive'
+            ? 'sentiment_satisfied'
+            : data.overall_sentiment_label === 'Negative'
+            ? 'sentiment_dissatisfied'
+            : 'sentiment_neutral';
+        const increasing =
+          data.overall_trend_label === 'Increasing' ? 'trending_up' : 'trending_down';
+        const sentimentWidgets: Widget[] = [
+          {
+            id: 1,
+            label: 'Total Videos Analyzed',
+            content: `${data.total_videos}`,
+            details: 'Videos in the last 30 days',
+            icon: 'play_circle',
+          },
+          {
+            id: 2,
+            label: 'Total Views (Aggregated)',
+            content: this._formatNumber(data.total_views),
+            details: 'Views across all analyzed videos',
+            icon: 'visibility',
+          },
+          {
+            id: 3,
+            label: 'Average Sentiment Score',
+            content: `${data.avg_sentiment.toFixed(2)} (${data.overall_sentiment_label})`,
+            details: 'Compared to last period',
+            icon: sentiment,
+          },
+          {
+            id: 4,
+            label: 'Trending Sentiment Direction',
+            content: data.overall_trend_label,
+            details: 'Compared to last period',
+            icon: increasing,
+          },
+        ];
+        const positiveCount = data.sentiment_distribution.positive;
+        const neutralCount = data.sentiment_distribution.neutral;
+        const negativeCount = data.sentiment_distribution.negative;
+        this.sentimentDistributions.set([positiveCount, neutralCount, negativeCount]);
+        this.widgets.set(sentimentWidgets);
+      });
+  }
+
+  private _getChartData(): void {
+    this._sentimentChartsSubscription = this._sentimentService.getSentimentData().subscribe({
+      next: (data) => {
+        // this.lineChartData.set(data.series);
+        this.lineChartData.set(MOCK_SERIES);
+        const uniqueCategories = [...new Set(MOCK_SERIES.map((item) => item.category_id))];
+        uniqueCategories.forEach((category) => {
+          const series = this._getSeriesByCategory(category);
+          if (category === uniqueCategories[0]) {
+            this.dataset1.set(series);
+          } else if (category === uniqueCategories[1]) {
+            this.dataset2.set(series);
+          } else if (category === uniqueCategories[2]) {
+            this.dataset3.set(series);
+          }
+        });
+        this.chartIsLoaded.set(true);
+      },
+      error: () => {
+        this.chartIsLoaded.set(false);
+      },
+    });
+  }
+
+  private _getSentimentVideos(): void {
+    this._positiveVideosSubscription = this._sentimentService
+      .getVideosBySentiment('positive')
+      .subscribe((data: SentimentVideoDto[]) => {
+        this.positiveVideos.set(data);
+        this._negativeVideosSubscription = this._sentimentService
+          .getVideosBySentiment('negative')
+          .subscribe((data: SentimentVideoDto[]) => {
+            this.negativeVideos.set(data);
+            this.videosAreLoaded.set(true);
+          });
+      });
+  }
+
+  private _getSeriesByCategory(categoryId: string): SentimentChartSeriesDto[] {
+    return this.lineChartData().filter((series) => series.category_id === categoryId);
+  }
+
+  private _formatNumber(value: number): string {
+    if (!value) {
+      return '';
+    }
+
+    if (value >= 1_000_000_000) {
+      return (value / 1_000_000_000).toFixed(1) + 'B';
+    }
+
+    if (value >= 1_000_000) {
+      return (value / 1_000_000).toFixed(1) + 'M';
+    }
+
+    if (value >= 1_000) {
+      return (value / 1_000).toFixed(1) + 'K';
+    }
+
+    return value.toString();
   }
 }
